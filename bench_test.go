b393df1facb5538a7459c7cb88ff55fd8a4e100f
William Poussier <william.poussier@gmail.com>
2022-03-25 14:47:07 +0100

    test: benchmark CompareJSON against Compare

--- bench_test.go
+++ bench_test.go
@@ -1,16 +1,27 @@
 package jsondiff
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"testing"
 )
 
-func BenchmarkCompareJSONOpts(b *testing.B) {
+func BenchmarkCompare(b *testing.B) {
 	beforeBytes, err := ioutil.ReadFile("testdata/benchs/before.json")
 	if err != nil {
 		b.Fatal(err)
 	}
+	var before interface{}
+	err = json.Unmarshal(beforeBytes, &before)
+	if err != nil {
+		b.Fatal(err)
+	}
 	afterBytes, err := ioutil.ReadFile("testdata/benchs/after.json")
+	if err != nil {
+		b.Fatal(err)
+	}
+	var after interface{}
+	err = json.Unmarshal(afterBytes, &after)
 	if err != nil {
 		b.Fatal(err)
 	}
@@ -27,7 +38,16 @@ func BenchmarkCompareJSONOpts(b *testing.B) {
 		{"factor+ratio", makeopts(Factorize(), Rationalize())},
 		{"all-options", makeopts(Factorize(), Rationalize(), Invertible())},
 	} {
-		b.Run(bb.name, func(b *testing.B) {
+		b.Run("Compare/"+bb.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				patch, err := CompareOpts(before, after, bb.opts...)
+				if err != nil {
+					b.Error(err)
+				}
+				_ = patch
+			}
+		})
+		b.Run("CompareJSON/"+bb.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				patch, err := CompareJSONOpts(beforeBytes, afterBytes, bb.opts...)
 				if err != nil {
